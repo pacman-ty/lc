091c64591c0dabb9a3d1466afc89c2c0ebfdfc57
Talha Yildirim <talha.yildirim720@gmail.com>
2026-01-18 10:29:49 -0500

    Create valid_parentheses.rs

---
+++ easy/valid_parentheses.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c { 
+                '{' => stack.push('}'),
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '}' | ')' | ']' if Some(c) != stack.pop() => return false,
+                _ => (),
+            }
+        }
+        stack.is_empty()
+    }
+}
